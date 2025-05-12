fd216809d59a104c4ea6c86c3dab13e882d73b29
汪正偉 <162937008+StevenWang1012@users.noreply.github.com>
2025-05-12 16:46:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
 
-# ⛓️ Notion API Token（固定名稱即可）
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
-
-# 📄 頁面 ID（用來更新區塊時間）
 PAGE_ID_MEETING_1 = os.environ["PAGEID_MEETING_1"]
-
-# 📊 資料庫 ID（用來統計 checkbox 進度）
 DATABASE_ID_MEETING_1 = os.environ["DBID_MEETING_1"]
